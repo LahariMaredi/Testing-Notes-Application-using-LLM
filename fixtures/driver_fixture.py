691600053e38882417185b9a2797680a04e26901
LahariMaredi <161640760+LahariMaredi@users.noreply.github.com>
2026-05-06 22:59:08 +0530

    Update driver_fixture.py

--- fixtures/driver_fixture.py
+++ fixtures/driver_fixture.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+import pytest
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
